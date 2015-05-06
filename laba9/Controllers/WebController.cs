96f361b36abd6fb038076b11af39a5ddbd59dbe1
AlbinaV <alv_t@mail.ru>
2015-05-07 03:14:18 +0500

    Web

---
+++ laba9/Controllers/WebController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using laba9.Models;
+
+
+namespace laba9.Controllers
+{
+    namespace WebServices.Controllers
+    {
+        public class WebController : ApiController
+        {
+            private rep repo = rep.Current;
+
+            public IEnumerable<order> GetAllOrders()
+            {
+                return repo.GetAll();
+            }
+
+            public order GetOrder(int id)
+            {
+                return repo.Get(id);
+            }
+
+             [HttpPost]
+            public order CreateOrder(order item)
+            {
+                return repo.Add(item);
+            }
+
+
+             [HttpPut]
+            public bool UpdateOrder(order item)
+            {
+                return repo.Update(item);
+            }
+
+            public void DeleteReservation(int id)
+            {
+                repo.Remove(id);
+            }
+        }
+    }
+}
---
+++ laba9/Models/rep.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace laba9.Models
+{
+    public class rep
+    {
+        private static rep repo = new rep();
+
+        public static rep Current
+        {
+            get
+            {
+                return repo;
+            }
+        }
+    
+        private List<order> data = new List<order> {
+            new order { 
+                OrderId = 1, Name = "Ipad", Count = 5, Address = "Ленина 89"},
+            new order { 
+                OrderId = 2, Name = "Iphone 6", Count = 10, Address = "Молодогвардейцев 102"},
+            new order { 
+                OrderId = 3, Name = "Macbook Air", Count = 1, Address = "Горького 10"},
+            new order { 
+                OrderId = 4, Name = "Macbook Pro", Count = 2, Address = "Горького 10"},
+    };
+
+        public IEnumerable<order> GetAll()
+        {
+            return data;
+        }
+
+        public order Get(int id)
+        {
+            return data.Where(r => r.OrderId == id).FirstOrDefault();
+        }
+
+        public order Add(order item)
+        {
+            item.OrderId = data.Count + 1;
+            data.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            order item = Get(id);
+            if (item != null)
+            {
+                data.Remove(item);
+            }
+        }
+
+        public bool Update(order item)
+        {
+            order storedItem = Get(item.OrderId);
+            if (storedItem != null)
+            {
+                storedItem.Name = item.Name;
+                storedItem.Count = item.Count;
+                storedItem.Address = item.Address;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
